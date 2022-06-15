6c7b4943e8a3969681bf59f80fb449653e98e806
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2022-06-15 14:53:29 -0700

    init commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.hasRead = hasRead;
+}
+
+Book.prototype.showInfo = function () {
+  let hasReadText = this.hasRead ? "read" : "unread";
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${hasReadText}`;
+};
+
+const book1 = new Book("Travels With Charley", "John Steinbeck", 244, true);
+console.log(book1.showInfo());
+
+function addBookToLibrary(book) {}
