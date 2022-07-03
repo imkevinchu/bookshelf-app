fe0aaa683b0502b655e4793fb95e1045c8f8a419
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2022-07-03 16:13:23 -0700

    toggling empty state

--- script.js
+++ script.js
@@ -21,10 +21,12 @@ class Bookshelf {
     if (!this.isInLibrary(newBook)) {
       this.books.push(newBook);
     }
+    toggleEmptyStateDisplay();
   }
 
   removeBook(title) {
     this.books = this.books.filter((book) => book.title !== title);
+    toggleEmptyStateDisplay();
   }
 
   getBook(title) {
@@ -34,19 +36,24 @@ class Bookshelf {
   isInLibrary(newBook) {
     return this.books.some((book) => book.title === newBook.title);
   }
+
+  isEmpty() {
+    return this.books.isEmpty();
+  }
 }
 
 const bookshelf = new Bookshelf();
 
-const book1 = new Book("Travels With Charley", "John Steinbeck", 244, true);
-bookshelf.addBook(book1);
-console.log(book1.showInfo());
+// const book1 = new Book("Travels With Charley", "John Steinbeck", 244, true);
+// bookshelf.addBook(book1);
+// console.log(book1.showInfo());
 
 const addBookBtn = document.getElementById("addBookBtn");
 const addBookSubmitBtn = document.getElementById("addBookSubmitBtn");
 const formContainer = document.getElementById("formContainer");
 const formOverlay = document.getElementById("formOverlay");
 const formCloseBtn = document.getElementById("close");
+const emptyState = document.getElementById("emptyState");
 
 const openAddBookForm = () => {
   formOverlay.classList.add("active");
@@ -56,4 +63,12 @@ const openAddBookForm = () => {
 const closeAddBookForm = () => {
   formOverlay.classList.remove("active");
   formContainer.classList.remove("active");
+};
+
+const toggleEmptyStateDisplay = () => {
+  if (bookshelf.isEmpty()) {
+    emptyState.classList.add("active");
+  } else {
+    emptyState.classList.remove("active");
+  }
 };
